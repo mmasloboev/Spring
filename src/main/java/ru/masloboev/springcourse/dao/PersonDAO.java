b71c5ca22e887ae13030b38e41580e1baf254f7d
unknown <masloboevmatvei@ya.ru>
2022-10-30 11:50:33 +0300

    crud with hibernate

--- src/main/java/ru/masloboev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/masloboev/springcourse/dao/PersonDAO.java
@@ -24,23 +24,32 @@ public class PersonDAO {
     @Transactional(readOnly = true)
     public List<Person> index() {
         Session session = sessionFactory.getCurrentSession();
-        List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
-        return people;
+        return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
     }
-
+    @Transactional
     public void save(Person person) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        person.setName(updatedPerson.getName());
+        person.setAge(updatedPerson.getAge());
+        person.setEmail(updatedPerson.getEmail());
     }
 
+    @Transactional
     public void delete(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(session.get(Person.class, id));
     }
 }
--- src/main/java/ru/masloboev/springcourse/models/Person.java
+++ src/main/java/ru/masloboev/springcourse/models/Person.java
@@ -20,13 +20,19 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @Email
+    @NotEmpty(message = "Email should not be empty")
+    @Column(name = "email")
+    private String email;
+
     public Person() {
 
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, String email) {
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -53,4 +59,22 @@ public class Person {
         this.age = age;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
